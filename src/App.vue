3d2371f94ea338f763ca9a4d7e6111449b37a80a
蔡昀芷 <yunchihlulu@gmail.com>
2019-09-20 13:09:57 +0800

    安裝 vue axios 並帶入專屬API

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    // 客戶端伺服器路徑 https://vue-course-api.hexschool.io/api/lulu7613/products
+    // 將會變動的 1.站點、2.API 設定到 config 的 dev.env.js
+    // process.env.API_PATH = https://vue-course-api.hexschool.io
+    // process.env.CUSTOM_PATH = lulu7613
+    const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`
+
+    this.$http.get(api).then(response => {
+      console.log(response.data)
+    })
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios' // 主要的 AJAX 套件
+import VueAxios from 'vue-axios' // 將 AJAX 轉為 Vue 的套件
+
 import App from './App'
 import router from './router'
 
+Vue.use(VueAxios, axios) // 執行 axios 套件
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
