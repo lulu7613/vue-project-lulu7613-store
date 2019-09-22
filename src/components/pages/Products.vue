c99fcec3383e2eed902e6b080a98855d20d99ded
蔡昀芷 <yunchihlulu@gmail.com>
2019-09-22 21:56:52 +0800

    不使用 bootstrap 的 jQuery，自定義 modal 觸發方法

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -2,7 +2,8 @@
 <template>
   <div>
     <div class="text-right my-3">
-      <button class="btn btn-primary">建立新的產品</button>
+      <!-- 製作 model 效果 -->
+      <button class="btn btn-primary" @click="openModal()">建立新的產品</button>
     </div>
     <table class="table table-hover">
       <!--
@@ -34,6 +35,34 @@
         </tr>
       </tbody>
     </table>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      :class="{ 'show': isShow }"
+      :style="{'display': showStyle.display, 'background-color': showStyle.backgroundColor}"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeModal()">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModal()">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,7 +71,13 @@ export default {
   data () {
     return {
       // 接收 ajax 的產品內容，提供各元件使用
-      products: []
+      products: [],
+      // modal 使用的樣式與判斷
+      isShow: false,
+      showStyle: {
+        display: '',
+        backgroundColor: ''
+      }
     }
   },
   methods: {
@@ -55,6 +90,22 @@ export default {
         // 存進 this.product
         vm.products = response.data.products
       })
+    },
+    // 開啟 modal 的觸發方法 (確保遠端資料已接收完畢，才能開啟 modal)
+    // 不使用 bootstrap 的 jQuery，自製觸發方式
+    openModal () {
+      this.isShow = true
+      if (this.isShow) {
+        this.showStyle.display = 'block'
+        this.showStyle.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+      }
+    },
+    closeModal () {
+      this.isShow = false
+      if (!this.isShow) {
+        this.showStyle.display = 'none'
+        this.showStyle.backgroundColor = ''
+      }
     }
   },
   created () {
